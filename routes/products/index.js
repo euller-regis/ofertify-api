3431ae9fc5b3673b1a373fa39f414b5cc2d7c1f1
euller <eullerpenaregis@gmail.com>
2025-06-12 17:12:16 -0300

    addition of the search, page and page_size parameters to the products page

--- routes/products/index.js
+++ routes/products/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { access } = require('fs');
 const { DatabaseSync } = require('node:sqlite');
 
 const database = new DatabaseSync(':memory:');
@@ -34,13 +35,34 @@ insertStatement.run('wood table', 1, 'retangular, 150 x 70cm, dark brown', 150, 
 insertStatement.run('simple office chair', 1, 'no-wheels, dark grey', 40, 'https://free-images.com/md/79f7/chair_garden_green_hedge.jpg');
 insertStatement.run('ergonomic office chair', 1, 'black', 80, 'https://free-images.com/md/b1c7/chair_office_table_workplace.jpg');
 
+for (let i = 0; i < 10000; i++ ){
+  insertStatement.run(`Product ${i+1}`, 1, '', 100, '');
+}
+
 
 module.exports = async function (fastify, opts) {
-    fastify.get('/', async function (request, reply) {
+  fastify.get('/', async function (request, reply) {
+    // destructuring
+    const { search, page, page_size } = request.query
+    console.log(request.query)
 
-        const query = database.prepare('SELECT product_name, id, price, image_url FROM products')
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header("Access-Control-Allow-Headers",  "*");
 
-        return query.all()
 
-    })
-  }
+      const query = database.prepare("SELECT product_name, id, price, image_url FROM products")
+      
+      const byName = query.all().product_name
+      console.log(byName)
+
+
+
+      const products = query.all().filter(product => !search || product.product_name.toLowerCase().includes(search.toLowerCase()))
+
+      const pg = Number(page)
+      const pgsz = Number(page_size)
+
+      return products.slice((pg - 1) * pgsz, (pgsz * pg) || 10)
+
+  })
+}
