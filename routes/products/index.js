133c5cb3be17427e3d70619c7ec11ce71151c52c
euller <eullerpenaregis@gmail.com>
2025-09-30 15:59:56 -0300

    added location and condition to the database and updated the reply

--- routes/products/index.js
+++ routes/products/index.js
@@ -18,8 +18,11 @@ database.exec(`
         CREATE TABLE products(
           product_name TEXT,
           id INTEGER PRIMARY KEY,
+          slug TEXT NOT NULL UNIQUE,
           category_id INTEGER NOT NULL,
           description TEXT,
+          condition TEXT,
+          location TEXT,
           price REAL,
           image_url TEXT,
           FOREIGN KEY(category_id) REFERENCES categories(id) 
@@ -32,36 +35,51 @@ const categoryData = database.prepare(
 categoryData.run("furniture");
 categoryData.run("eletronics");
 categoryData.run("books");
+categoryData.run("general");
 
 const insertStatement = database.prepare(
-    "INSERT INTO products (product_name, category_id, description, price, image_url) VALUES (?, ?, ?, ?, ?)"
+    "INSERT INTO products (product_name, category_id, slug, description, condition, location, price, image_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 );
 insertStatement.run(
     "wood table",
     1,
+    "wood_table_12345",
     "retangular, 150 x 70cm, dark brown",
-    150,
+    "new",
+    "São Paulo",
+    150.5,
     "https://free-images.com/md/ecf2/wood_table_chairs_bench.jpg"
 );
 insertStatement.run(
     "simple office chair",
     1,
+    "simple_office_chair_12345",
     "no-wheels, dark grey",
-    40,
+    "new",
+    "Campinas",
+    40.99,
     "https://free-images.com/md/79f7/chair_garden_green_hedge.jpg"
 );
 insertStatement.run(
     "ergonomic office chair",
     1,
+    "ergonomic_office_chair_12345",
     "black",
-    80,
+    "semi-new",
+    "São Paulo",
+    80.0,
     "https://free-images.com/md/b1c7/chair_office_table_workplace.jpg"
 );
 
 for (let i = 0; i < 50; i++) {
+    const pname = faker.commerce.product();
+    const pslug = pname.replace(" ", "_");
     insertStatement.run(
-        faker.commerce.product(),
-        1,
+        pname,
+        4,
+        `${pslug}_${faker.string.alphanumeric(5)}`,
+        "",
+        "",
         "",
         faker.number.float({ min: 10, max: 1000, fractionDigits: 2 }),
         ""
@@ -83,7 +101,7 @@ module.exports = async function (fastify, opts) {
         reply.header("Access-Control-Allow-Headers", "*");
 
         const query = database.prepare(
-            "SELECT product_name, id, price, image_url FROM products"
+            "SELECT product_name, slug, price, image_url, category_id FROM products"
         );
 
         const prod = sortProducts(
@@ -114,5 +132,23 @@ module.exports = async function (fastify, opts) {
                 { value: "priceDSC", label: "highest price" },
             ],
         };
+    });
+
+    fastify.get("/:slug", async function (request, reply) {
+        reply.header("Access-Control-Allow-Origin", "*");
+        reply.header("Access-Control-Allow-Headers", "*");
+
+        const { slug } = request.params;
+        const query = database.prepare(
+            "SELECT product_name, description, condition, location, category_id, price, image_url FROM products WHERE slug = ?"
+        );
+
+        const product = query.get(slug);
+
+        if (product) {
+            return product;
+        }
+
+        reply.statusCode = 404;
     });
 };
